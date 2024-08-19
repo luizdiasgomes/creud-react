9c26e87ebc16b59fad75c09da6dd89ecc2da0a41
Luiz Fernando Dias Gomes <luizdiasgomes2@gmail.com>
2024-08-19 08:38:17 -0300

    Primeiro commit

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import Main from '../template/Main'
+import React from "react";
+
+export default props => 
+    <Main icon="home" title="Início"
+        subtitle="Projeto CRUD usando React.">
+        <div className="display-4">Bem vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema para exemplificar a contrucao de um cadastro desenvolvido em React!</p>
+    </Main>
